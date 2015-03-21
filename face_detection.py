b36baab0dea02d608317abf7e44e94aa8b5f29af
He Li <li522@purdue.edu>
2015-03-21 17:14:33 -0400

    show face region

--- face_detection.py
+++ face_detection.py
@@ -8,14 +8,15 @@ def main():
 		ret, frame = cap.read()
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+		face = None
 		for (x, y, w, h) in faces:
-			cv2. rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+			face = cv2. rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 			roi_gray = gray[y:y+h, x:x+w]
 			roi_color = frame[y:y+h, x:x+w]
 			#eyes = eye_cascade.detectMultiScale(roi_gray)
 			#for (ex,ey,ew,eh) in eyes:
 				#cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
-		
+			cv2.imshow('face', face)
 		cv2.imshow('frame', frame)
 		
 		k = cv2.waitKey(30) & 0xff
